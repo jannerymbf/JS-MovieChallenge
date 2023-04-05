aeb80f979d455ba09bab04ffc411250b4b23cac0
Jannery Briceño <jannerymai@gmail.com>
2023-04-05 13:55:28 -0500

    Add interaction to Search button

--- src/movie.js
+++ src/movie.js
@@ -1,8 +1,24 @@
 import { gettingMovies } from "./main.js";
 
-const input = document.getElementById('number');
+const input = document.getElementById('movie-title');
 const btn = document.getElementById('search-btn');
+const moviesContainer = document.getElementById('movies-container');
 
-// btn.addEventListener('click', () => {
-//   // gettingData(input.value);
-// })
+btn.addEventListener('click', () => {
+  let template = '';
+  gettingMovies(input.value)
+    .then(response => {
+      response.forEach(element => {
+        template +=
+          `<article>
+            <img width=100 src="${element.Poster}" alt="${element.Title}">
+            <div>
+              <p>${element.Title}</p>
+              <p>${element.Type}</p>
+              <p>${element.Year}</p>
+            </div>
+          </article>`
+      });
+      moviesContainer.innerHTML = template;
+    });
+})
