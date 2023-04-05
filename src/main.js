5f3c0f2692397566763b05eb476603e076272c41
Jannery Briceño <jannerymai@gmail.com>
2023-04-05 13:18:40 -0500

    Add basic html layout

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
-export const example = () => {
-  return 'example';
-};
+// export async function gettingData(movie) {
+//   const response = await fetch(`http://www.omdbapi.com/?t=${movie.title}&plot=full&apikey=e76bd81`);
+//   const data = await response.json();
+//   console.log(data);
+// }
+
+// gettingData({title: 'harry potter'})
---
+++ src/movie.js
@@ -0,0 +1,8 @@
+// import { gettingData } from "./main.js";
+
+// const input = document.getElementById('number');
+// const btn = document.getElementById('search-btn');
+
+// btn.addEventListener('click', () => {
+//   // gettingData(input.value);
+// })
