09c9a23e543f0379c38cd5058a420c7740f8b29f
Jannery Briceño <jannerymai@gmail.com>
2023-04-11 18:22:26 -0500

    Add styles to movie gallery section

--- src/movie.js
+++ src/movie.js
@@ -16,8 +16,8 @@ btn.addEventListener('click', () => {
           `<a href="#movie-display">
             <article class="${element.imdbID}">
               <img class="${element.imdbID}" width=100 src="${element.Poster}" alt="${element.Title}">
-              <div class="${element.imdbID}">
-                <p class="${element.imdbID}">${element.Title}</p>
+              <div class="${element.imdbID} gallery__display__movies__movie-info">
+                <p class="${element.imdbID} gallery__display__movies__movie-info--title">${element.Title}</p>
                 <p class="${element.imdbID}">${element.Type}</p>
                 <p class="${element.imdbID}">${element.Year}</p>
               </div>
